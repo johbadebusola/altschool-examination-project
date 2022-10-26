85196fa9cf9b7b368022dac977e696267e6afbb2
Oluwatoyin John Adebusola <101136827+johbadebusola@users.noreply.github.com>
2022-10-26 18:32:16 +0100

    added authentication

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,5 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import { signInWithGoogle ,GoogleAuthProvider} from "./firebase"
 
 import img2 from "./img2.svg"
 import img3 from "./img3.svg"
@@ -8,38 +8,68 @@ import img6 from "./img6.svg"
 import img7 from "./img7.svg"
 import "./index.css"
  const Home = () => {
- 
+const [name,setName] = useState("user")
+  const signIn = () => {
+    signInWithGoogle()
+    .then((result) => {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+      // The signed-in user info.
+      const user = result.user;
+      console.log(user.email )
+      console.log(user.displayName)
+      setName(user)
+      // ...
+    }).catch((error) => {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // The email of the user's account used.
+      const email = error.customData.email;
+      // The AuthCredential type that was used.
+      const credential = GoogleAuthProvider.credentialFromError(error);
+      // ...
+    });
+  } 
+  console.log(name.email)
   return (
     <>
+    <div>
+      <button onClick={signIn} >
+        Sign in with google
+      </button>
+    </div>
+    <h5> Hi, {name.displayName} </h5>
     <h4 className='home-head'> Click on the user tab to view the list of people who uses our service.</h4>
    <div className='home-img'>
     <div>
-    <img  className='img1' src={img7}  alt="image"/>
+    <img  className='img1' src={img7}  alt="person1"/>
     <p className='home-p'> The listed Users of the account can like messages from other user</p>
     </div>
 
     <div>
-    <img  className='img1' src={img2} alt="image"/>
+    <img  className='img1' src={img2} alt="person2"/>
     <p className='home-p'> The listed Users of the account can send emails to eachother </p>
     </div>
 
     <div>
-    <img  className='img1' src={img3} alt="image"/>
+    <img  className='img1' src={img3} alt="person3"/>
     <p className='home-p'> The listed Users of the account can send emails to eachother </p>
     </div>
 
     <div>
-    <img  className='img1' src={img4} alt="image" />
+    <img  className='img1' src={img4} alt="person4" />
     <p className='home-p'> Feedback from our users is also required for better services</p>
     </div>
 
     <div>
-    <img  className='img1' src={img6} alt="image"/>
+    <img  className='img1' src={img6} alt="person5"/>
     <p className='home-p'> The listed Users can also give a review of the product purchased or services rendered </p>
     </div>
 
     <div>
-    <img  className='img1' src={img7} alt="image" />
+    <img  className='img1' src={img7} alt="person6" />
     <p className='home-p'> The listed Users of the account can send emails to eachother </p>
     </div>
 
---
+++ src/firebase.js
@@ -0,0 +1,36 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+import {getAuth,GoogleAuthProvider, signInWithPopup} from "firebase/auth"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAXWhmB-s6QWtV_gn6uZ_ILkgTP5B2rPvU",
+  authDomain: "userxifyy.firebaseapp.com",
+  projectId: "userxifyy",
+  storageBucket: "userxifyy.appspot.com",
+  messagingSenderId: "834906190931",
+  appId: "1:834906190931:web:8f437cdd39a5bc2318c2a6",
+  measurementId: "G-F2G86M53CP"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+const provider = new GoogleAuthProvider()
+
+const signInWithGoogle = () => signInWithPopup(auth, provider)
+ 
+
+
+
+export {
+  auth,
+  signInWithGoogle,
+  GoogleAuthProvider
+}
